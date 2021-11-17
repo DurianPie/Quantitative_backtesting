aa3930c481c5fe24f0105c64e26af5651c9be5ff
杜惟康 <duweikang@pku.edu.cn>
2021-11-17 20:08:48 +0800

    test

---
+++ src/stock_visualize.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pandas
+import pickle
+import matplotlib as plt
+
+"""
+    预计主要使用到ploty, plty.express , mlpfinance当然还少不了matplotlib
+"""
+class stock_visualize():
+    intro  = '用于个股和组合的可视化分析'
+
+    def simple_one_stock(name, price, time,*args):
+        pass
